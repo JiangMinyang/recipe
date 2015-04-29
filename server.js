166a2a86bd073a3ea511917b2f1278d029d28b1a
JiangMinyang <jiangminyangcn@gmail.com>
2015-04-29 21:01:33 +0000

    add route

--- server.js
+++ server.js
@@ -1,48 +1,67 @@
 var hapi = require('hapi');
 var qstring = require('querystring');
+var mongoose = require('mongoose');
 var server = new hapi.Server();
-server.connection({port : 3000});
+
+var db = mongoose.connect('mongodb://localhost/test');
+var recipeSchema = require('./recipe_schema.js').recipeSchema;
+var Recipe = mongoose.model('Recipe', recipeSchema, 'recipe');
+
+server.connection({port : 8080});
+
 
 server.route({
 	method	:	"GET",
 	path	:	"/api/recipe/{id}",
 	handler	:	function(request, response) {
-		response("hello");
+		/*
+		var ID = request.params.id;
+		console.log(ID);
+		var query = Recipe.find().where('_id', ID);
+		query.exec(function(err, doc) {
+			if (err) return console.log(err);
+			response(doc, 200);
+		});
+		*/
 	}
 });
 server.route({
 	method	:	"GET",
 	path	:	"/api/recipe",
 	handler	:	function(request, response) {
-		console.log(request.query);
+		//console.log(request.query);
 		response("hello");
 	}
 });
-/*
+
 server.route({
-	method	:	"GET",
-	path	:	"/",
+	method	:	"POST",
+	path	:	"/api/recipe",
+	handler	:	function(request, response) {
+		var query = request.query;
+		console.log(query);
+		response('!!!');
+	}
+
+});
+
+server.route({
+	method	:	"PUT",
+	path	:	"/api/recipe/{id}",
 	handler	:	function(request, response) {
 
 	}
 
 });
 server.route({
-	method	:	"GET",
-	path	:	"/",
-	handler	:	function(request, response) {
-
-	}
-
-});
-server.route({
-	method	:	"GET",
-	path	:	"/",
+	method	:	"DELETE",
+	path	:	"/api/recipe/{id}",
 	handler	:	function(request, response) {
 
 	}
 });
-*/
+
+
 server.start(function() {
 	console.log("Server running at: ", server.info.uri);
 });
