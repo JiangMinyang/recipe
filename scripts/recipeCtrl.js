fcb2b6ce34b105968952ab427c59da56aa81a378
Minyang Jiang <jiangminyangcn@gmail.com>
2015-05-04 14:24:29 -0700

    change to localhost

--- scripts/recipeCtrl.js
+++ scripts/recipeCtrl.js
@@ -1,12 +1,12 @@
 var app = angular.module('recipeManager', []);
 
 app.controller('recipeCtrl', function($scope, $http){
-	$http.get("http://192.168.33.10:8080/api/recipe?page=1").success(function(response, status) {
+	$http.get("http://localhost:8080/api/recipe?page=1").success(function(response, status) {
 		console.log(response);
 		$scope.searchResults = response;
 	})
 	$scope.Search = function() {
-		var urlStr = "http://192.168.33.10:8080/api/recipe?";
+		var urlStr = "http://localhost:8080/api/recipe?";
 		if (typeof($scope.title) != 'undefined') urlStr = urlStr + 'title=' + $scope.title;
 		if (typeof($scope.tags) != 'undefined') {
 			var tag = $scope.tags.split(',');
@@ -24,7 +24,7 @@ app.controller('recipeCtrl', function($scope, $http){
 	}
 
 	$scope.sortByKey = function() {
-		var urlStr = "http://192.168.33.10:8080/api/recipe?";
+		var urlStr = "http://localhost:8080/api/recipe?";
 		if (typeof($scope.title) != 'undefined') urlStr = urlStr + 'title=' + $scope.title;
 		if (typeof($scope.tags) != 'undefined') {
 			var tag = $scope.tags.split(',');
